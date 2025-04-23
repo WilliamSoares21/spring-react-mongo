a4cff39fcd827c6cd2a8deb558b9b24460658b65
William Soares <pedro.williamsoares2022@gmail.com>
2025-04-23 14:41:01 -0300

    Foi feito o inicio da estruturação do projeto com spring boot

---
+++ spring-react-mongo/src/main/java/org/will/spring_react_mongo/SpringReactMongoApplication.java
@@ -0,0 +1,21 @@
+package org.will.spring_react_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringReactMongoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringReactMongoApplication.class, args);
+
+  }
+
+  @GetMapping("/")
+  public String apiRoat() {
+    return "Hello World!";
+  }
+}
---
+++ spring-react-mongo/src/test/java/org/will/spring_react_mongo/SpringReactMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.will.spring_react_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
