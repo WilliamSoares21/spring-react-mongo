f05bace5c1e88e53032cfd3fed1292e62569cb96
William Soares <pedro.williamsoares2022@gmail.com>
2025-04-24 14:24:51 -0300

    Atualizei as classes Review e Movie para o padrão do repositório base

--- spring-react-mongo/src/main/java/org/will/spring_react_mongo/Movie.java
+++ spring-react-mongo/src/main/java/org/will/spring_react_mongo/Movie.java
@@ -1,38 +1,41 @@
 package org.will.spring_react_mongo;
 
-import java.util.List;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
-
   @Id
-  private Object id;
-
+  private ObjectId id;
   private String imdbId;
-
   private String title;
-
   private String releaseDate;
-
   private String trailerLink;
-
   private String poster;
-
   private List<String> genres;
-
   private List<String> backdrops;
 
   @DocumentReference
-  private List<Review> reviewIds;
+  private List<Review> reviews;
+
+  public Movie(String imdbId, String title, String releaseDate, String trailerLink,
+      String poster, List<String> genres, List<String> backdrops) {
+    this.imdbId = imdbId;
+    this.title = title;
+    this.releaseDate = releaseDate;
+    this.trailerLink = trailerLink;
+    this.poster = poster;
+    this.genres = genres;
+    this.backdrops = backdrops;
+  }
 }
--- spring-react-mongo/src/main/java/org/will/spring_react_mongo/MovieController.java
+++ spring-react-mongo/src/main/java/org/will/spring_react_mongo/MovieController.java
@@ -1,24 +1,26 @@
 package org.will.spring_react_mongo;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
-
   @Autowired
   private MovieService movieService;
 
   @GetMapping
   public ResponseEntity<List<Movie>> getAllMovies() {
-    return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    return new ResponseEntity<>(movieService.findAllMovies(), HttpStatus.OK);
   }
 
+  @GetMapping("/{imdbId}")
+  public ResponseEntity<Optional<Movie>> getMovieByImdbId(@PathVariable String imdbId) {
+    return new ResponseEntity<>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
+  }
 }
