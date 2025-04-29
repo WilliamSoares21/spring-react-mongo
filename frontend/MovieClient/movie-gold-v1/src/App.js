182ca442d5255c826fb50e42e58a4e110180f3af
William Soares <pedro.williamsoares2022@gmail.com>
2025-04-29 14:40:19 -0300

    Iniciado a estrutura de home e apresentação de filmes do frontend

--- frontend/MovieClient/movie-gold-v1/src/App.js
+++ frontend/MovieClient/movie-gold-v1/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+
+
+
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -44,7 +50,13 @@ function App() {
           </div>
         );
       })}
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
+    </div >
   );
 }
 
